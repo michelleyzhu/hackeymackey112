93e15568361854fd0b361329427f0ba1e61de981
sophie006liu <41531009+sophie006liu@users.noreply.github.com>
2020-11-07 13:35:02 -0800

    Update makeEmail.py
    made a template

--- makeEmail.py
+++ makeEmail.py
@@ -1,132 +1,51 @@
-#get the information for the blanks
-
-#put the information in the blank
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import urllib.parse
-import urllib.request
-import re
-
-
-try: 
-    from googlesearch import search 
-except ImportError:  
-    print("No module named 'google' found") 
-  
-alphabets= "([A-Za-z])"
-prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-websites = "[.](com|net|org|io|gov)"
-
-def split_into_sentences(text):
-    text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    text = text.replace(".",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
-    sentences = text.split("<stop>")
-    sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
-    return sentences
-
-# to search
-def getURLs(q): 
-    query = q
-    sites = set()
-    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-        sites.add(j)
-    return sites 
-
-#looks through all the sites makes dictionary of sources to key quotes
-def lookThroughSources(sites):
-    sentenceDict = dict()
-    for address in sites:
-        text = getPageText(address)
-        if text == None:
-            continue
-        searchPageText(text, sentenceDict, address)
-    print(sentenceDict)
-    
-def getPageText(address):
-    url = address
-    #html = urlopen(url).read()
-    #soup = BeautifulSoup(html, features="html.parser")
-
-    values = {'language': 'Python' }
-    headers = {'User-Agent': "Mozilla/5.0"}
-
-    data = urllib.parse.urlencode(values)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data, headers)
-    try:
-        response = urllib.request.urlopen(req)
-        html = response.read()
-        soup = BeautifulSoup(html, features="html.parser")
-
-        # kill all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()    # rip it out
-
-        # get text
-        text = soup.get_text()
-
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-    except:
-        return
-    return text
-
-
-#retrieve the notable quotes
-def searchPageText(text, localDict, address):
-    sentences = split_into_sentences(text)
-    sourceSet = set()
-    for element in sentences:
-        if len(element.split(" ")) < 6:
-            continue
-        else:
-            if re.finditer('according to', element):
-                sourceSet.add(element)
-            if re.finditer('%', element) or re.finditer('percent', element):
-                sourceSet.add(element)
-            if re.finditer('because', element):
-                sourceSet.add(element)
-    localDict[address] = sourceSet
-
-
-
+from getStatistics import *
+import random
 sourcesSet = getURLs("abortion bad") 
-lookThroughSources(sourcesSet)
+sourceQuoteDict = lookThroughSources(sourcesSet)
 
 
+zipcode = 1
+name = 2
+state = 3
+issue = 4
+personemail = 5
+senator = 6
+
+#i need
+statusAbout = 7
+stance = 8
+source1, content1 = generateSources(sourceQuoteDict)
+source2, content2 = generateSources(sourceQuoteDict)
+
+
+
+def generateSources(sourceQuoteDict):
+    source1 = random.choice(list(d.items()))
+    randSourceEntry = random.choice(list(capital))
+    return (source1, randSourceEntry)
+
+
+
+emailText = f'''Dear Congress Person {senator}:
+
+My name is {name} and I reside at [Insert Your Address] in [Insert Your City], South Carolina.  I am {statusAbout}.  
+
+
+I am writing you about {issue}, and asking that you stand {stance} it. 
+
+This issue is important to me because:
+According to {source1}, \" {content1}\"
+And according to {source2}, \" {content2}\"
+
+I appreciate your help and ask that you please send me a response letting me know if you are able to pass a Bill that would improve lives of American citizens like me.
+
+
+Thank you for your time and considering my request.
+
+
+Sincerely,
+
+{name}
 '''
-Hello my name is ____. I live in ______. 
-
-I noticed on this_____ you voted this______. This matters to me a lot, and I believe ______.
 
 
-
-I am emailing you about ______. Here is why this issue is important:
-Generate 6 statistics
-They pick 3 that speak to them'''
