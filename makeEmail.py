2526c10c5892e50b0e2d1b197a9f17ef78a10578
Michelle Li <Michelle@MacBook-Pro-8.local>
2020-11-07 16:06:28 -0600

    new

--- makeEmail.py
+++ makeEmail.py
@@ -27,25 +27,13 @@ source2, content2 = generateQuoteAndSource("abortion")
 
 
 emailText = f'''Dear Congress Person {senator}:
-
 My name is {name} and I reside at [Insert Your Address] in [Insert Your City], South Carolina.  I am {statusAbout}.  
-
-
 I am writing you about {issue}, and asking that you stand {stance} it. 
-
 This issue is important to me because:
 According to {source1}, \" {content1}\"
 And according to {source2}, \" {content2}\"
-
 I appreciate your help and ask that you please send me a response letting me know if you are able to pass a Bill that would improve lives of American citizens like me.
-
-
 Thank you for your time and considering my request.
-
-
 Sincerely,
-
 {name}
 '''
-
-
