8a02c4c753434b399742379ae53a8db28165b6b2
michellezhu <huaerzhu@gmail.com>
2020-11-07 01:32:02 -0500

    fixed syntax on scraping-reps

--- scraping-reps.py
+++ scraping-reps.py
@@ -1,7 +1,6 @@
 #for finding reps and their emails
 import requests
-from bs4 import BeautifulSoup4
-soup = BeautifulSoup(html_doc, 'html.parser')
+from bs4 import BeautifulSoup
 
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -11,8 +10,11 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
-def find-senators(state):
-    senators-url = "https://en.wikipedia.org/wiki/List_of_current_United_States_senators"
-    req = requests.get(senators-url, headers)
+def findSenators(state):
+    senatorsUrl = "https://en.wikipedia.org/wiki/List_of_current_United_States_senators"
+    req = requests.get(senatorsUrl, headers)
     soup = BeautifulSoup(req.content, 'html.parser')
     print(soup.prettify())
+
+
+findSenators("yay")
